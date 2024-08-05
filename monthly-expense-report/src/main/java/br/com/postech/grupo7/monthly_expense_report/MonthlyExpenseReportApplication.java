63d5780067612749b4f1c9b0a61fb80a618b3f3f
Iago Gomes <iagoomes@outlook.com>
2024-08-05 20:30:15 -0300

    chore(API): Initial commit
    techChallange: 1

---
+++ monthly-expense-report/src/main/java/br/com/postech/grupo7/monthly_expense_report/MonthlyExpenseReportApplication.java
@@ -0,0 +1,13 @@
+package br.com.postech.grupo7.monthly_expense_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlyExpenseReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlyExpenseReportApplication.class, args);
+	}
+
+}
---
+++ monthly-expense-report/src/test/java/br/com/postech/grupo7/monthly_expense_report/MonthlyExpenseReportApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postech.grupo7.monthly_expense_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthlyExpenseReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
