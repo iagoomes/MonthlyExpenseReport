859d594159974946cdd71bd37b9b39afbe662721
Iago Gomes <iagoomes@outlook.com>
2024-08-05 20:51:51 -0300

    refactor(API): refactor package
    techChallange: 1

--- monthly-expense-report/src/test/java/br/com/postech/grupo7/monthlyexpensereport/MonthlyExpenseReportApplicationTests.java
+++ monthly-expense-report/src/test/java/br/com/postech/grupo7/monthlyexpensereport/MonthlyExpenseReportApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.postech.grupo7.monthly_expense_report;
+package br.com.postech.grupo7.monthlyexpensereport;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
