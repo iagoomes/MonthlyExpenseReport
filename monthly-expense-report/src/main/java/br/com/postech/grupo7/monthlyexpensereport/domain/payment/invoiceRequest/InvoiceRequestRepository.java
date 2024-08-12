9c2acc1351af3d37c6c29583a94410779162e6f9
Iago Gomes <iagoomes@outlook.com>
2024-08-12 14:41:51 -0300

    feat(invoice-request): merge InvoiceRequestController into PaymentController and create DTO and Repository
    - Remove `InvoiceRequestController` and merge its functionality into `PaymentController`.
    - Create `InvoiceRequestDTO` to handle invoice request data transfer.
    - Create `InvoiceRequestRepository` to manage invoice request persistence.
    - Update `PaymentController` to handle invoice request operations.

--- monthly-expense-report/src/main/java/br/com/postech/grupo7/monthlyexpensereport/domain/payment/invoiceRequest/InvoiceRequestRepository.java
+++ monthly-expense-report/src/main/java/br/com/postech/grupo7/monthlyexpensereport/domain/payment/invoiceRequest/InvoiceRequestRepository.java
@@ -1,4 +1,4 @@
-package br.com.postech.grupo7.monthlyexpensereport.domain.payment;
+package br.com.postech.grupo7.monthlyexpensereport.domain.payment.invoiceRequest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
